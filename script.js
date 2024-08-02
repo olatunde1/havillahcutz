062848ad2d907fb440a9f2ac5946132acc994024
Adetunji <idowuolatunde32@gmail.com>
2024-08-02 18:27:54 +0100

    slider added to the landing page

--- script.js
+++ script.js
@@ -67,3 +67,44 @@ function Delete() {
 
 // Start the typing effect on load
 _INTERVAL_VAL = setInterval(Type, 100);
+
+
+
+
+var swiper = new Swiper(".blog-slider", {
+	loop: true,
+	slidesPerView: "1",
+	speed: 8000,
+	effect: "coverflow",
+	coverflowEffect: {
+		slideShadows: false,
+	},
+	mousewheel: {
+		invert: false,
+	},
+	autoplay: {
+		delay: 3000,
+	},
+	breakpoints: {
+		0: {
+			effect: "slide",
+			centeredSlides: false,
+		},
+		768: {
+			slidesPerView: "2",
+			centeredSlides: true,
+		},
+		1200: {
+			slidesPerView: "3",
+			centeredSlides: true,
+		}
+	},
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+		type: "fraction"
+	},
+
+});
+
+
